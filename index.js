045159fc08ccda793b1c030394c19355bf22d371
William Poynter <poynter.william@gmail.com>
2016-01-27 15:44:29 +0000

    Added port configuration

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var io = require('socket.io').listen(80);
+var io = require('socket.io').listen( process.env.PORT || 5000);
 
 io.on('connection', function(socket) {
 	console.log('a user connected');
