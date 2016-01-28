26a46f2d847b8d04b4b1c20527f50eaddbee1f2e
William Poynter <poynter.william@gmail.com>
2016-01-28 16:33:09 +0000

    Initial development

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-var io = require('socket.io').listen( process.env.PORT || 5000);
+var io = require('socket.io')
+	.listen( process.env.PORT || 5000),
+redis = require('redis').createClient( process.env.REDIS_URL || '' );
+
+redis.subscribe('rt-update');
 
 io.on('connection', function(socket) {
 	console.log('a user connected');
+	redis.on('message', function(channel, message) {
+		console.log(message)
+		socket.emit('rt-update', message);
+	});
 });
 
